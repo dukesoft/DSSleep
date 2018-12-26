e2943d55761203b61d38b9d8969e2108d48871c0
RobQuistNL <rob.quist@dukesoft.nl>
2018-12-26 05:02:12 +0100

    Update linux builder

--- src/build_linux.sh
+++ src/build_linux.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-gcc -sharedlib main.cpp -o bin/Release/DSSleep.so
+# gcc -sharedlib main.cpp -o bin/Release/DSSleep.so
+
+clang++ -std=c++11 -stdlib=libstdc++ -m32 -Wall -Wno-unknown-pragmas -fPIC -c DSSleep.cpp
+clang++ -m32 -shared -Wl,-soname,libDSSleep.so.0 -Wl,-rpath,"\$ORIGIN" -o libDSSleep.so DSSleep.o -L.
