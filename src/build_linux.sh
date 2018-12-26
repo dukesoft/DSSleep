93beb8f9a659f8f7da2f23e03a36c7251d41d53e
RobQuistNL <rob.quist@dukesoft.nl>
2018-12-26 05:08:17 +0100

    Update linux builder

--- src/build_linux.sh
+++ src/build_linux.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 # gcc -sharedlib main.cpp -o bin/Release/DSSleep.so
 
+mingw32-g++.exe -std=c++11 -Wall -DBUILD_DLL -O2  -c DSSleep.cpp -o obj\Release\DSSleep.o
+mingw32-g++.exe -shared -Wl,--output-def=bin\Release\libDSSleep.def -Wl,--out-implib=bin\Release\libDSSleep.a -Wl,--dll  obj\Release\DSSleep.o  -o bin\Release\DSSleep.dll -s  -luser32
+
+
 clang++ -std=c++11 -stdlib=libstdc++ -m32 -Wall -Wno-unknown-pragmas -fPIC -c DSSleep.cpp
 clang++ -m32 -shared -Wl,-soname,libDSSleep.so.0 -Wl,-rpath,"\$ORIGIN" -o libDSSleep.so DSSleep.o -L.
