6b68826d452bd41e3a9be1dec2b7780d6fc5c1c9
RobQuistNL <rob.quist@dukesoft.nl>
2018-12-26 05:30:08 +0100

    Fixed linux build

--- src/build_linux.sh
+++ src/build_linux.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
-# gcc -sharedlib main.cpp -o bin/Release/DSSleep.so
-
-#g++ -std=c++11 -Wall -c DSSleep.cpp -o obj\Linux\Release\DSSleep.o
-#g++ -shared -Wl,-soname,bin\Linux\Release\libDSSleep -Wl,--out-implib=bin\Linux\Release\libDSSleep.a -Wl,--dll  obj\Linux\Release\DSSleep.o  -o bin\Linux\Release\DSSleep.dll -s  -luser32
-
-
-#g++ -std=c++11 -stdlib=libstdc++ -m32 -Wall -Wno-unknown-pragmas -fPIC -c DSSleep.cpp
-#g++ -m32 -shared -Wl,-soname,libDSSleep.so.0 -Wl,-rpath,"\$ORIGIN" -o libDSSleep.so DSSleep.o -L.
-
-#g++ -std=c++11 -static-libstdc++ -m32 -Wall -fPIC -c DSSleep.cpp
-#g++ -m32 -shared -Wl,-soname,libDSSleep.so.0 -Wl,-rpath,"\$ORIGIN" -o libDSSleep.so DSSleep.o -L.
-
 g++ -Wall -fPIC -c DSSleep.cpp
 g++ -shared -Wl,-soname,DSSleep.so -o DSSleep.so *.o
 rm *.o
