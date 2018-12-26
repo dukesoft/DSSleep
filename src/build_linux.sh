5a1fabf397217600ef504f8fcb00c5654578b71f
Rob Quist <rob.quist@dukesoft.nl>
2018-12-26 05:18:16 +0100

    Working linux builder

--- src/build_linux.sh
+++ src/build_linux.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 # gcc -sharedlib main.cpp -o bin/Release/DSSleep.so
 
-mingw32-g++.exe -std=c++11 -Wall -DBUILD_DLL -O2  -c DSSleep.cpp -o obj\Release\DSSleep.o
-mingw32-g++.exe -shared -Wl,--output-def=bin\Release\libDSSleep.def -Wl,--out-implib=bin\Release\libDSSleep.a -Wl,--dll  obj\Release\DSSleep.o  -o bin\Release\DSSleep.dll -s  -luser32
+#g++ -std=c++11 -Wall -c DSSleep.cpp -o obj\Linux\Release\DSSleep.o
+#g++ -shared -Wl,-soname,bin\Linux\Release\libDSSleep -Wl,--out-implib=bin\Linux\Release\libDSSleep.a -Wl,--dll  obj\Linux\Release\DSSleep.o  -o bin\Linux\Release\DSSleep.dll -s  -luser32
 
 
-clang++ -std=c++11 -stdlib=libstdc++ -m32 -Wall -Wno-unknown-pragmas -fPIC -c DSSleep.cpp
-clang++ -m32 -shared -Wl,-soname,libDSSleep.so.0 -Wl,-rpath,"\$ORIGIN" -o libDSSleep.so DSSleep.o -L.
+#g++ -std=c++11 -stdlib=libstdc++ -m32 -Wall -Wno-unknown-pragmas -fPIC -c DSSleep.cpp
+#g++ -m32 -shared -Wl,-soname,libDSSleep.so.0 -Wl,-rpath,"\$ORIGIN" -o libDSSleep.so DSSleep.o -L.
+
+#g++ -std=c++11 -static-libstdc++ -m32 -Wall -fPIC -c DSSleep.cpp
+#g++ -m32 -shared -Wl,-soname,libDSSleep.so.0 -Wl,-rpath,"\$ORIGIN" -o libDSSleep.so DSSleep.o -L.
+
+g++ -Wall -fPIC -c DSSleep.cpp
+g++ -shared -Wl,-soname,libdssleep.so.1 -o libdssleep.so.1.0 *.o
