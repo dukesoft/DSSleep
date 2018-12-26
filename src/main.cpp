135bbea046e7a59c47084e6456f07ef8bb87976b
RobQuistNL <rob.quist@dukesoft.nl>
2018-12-26 04:53:19 +0100

    Fix DLL source

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,37 @@
-#include "main.h"
+#include <thread>
+#include <chrono>
 
-DLL_EXPORT double sleep_nanoseconds(double nanoseconds) {
-	std::this_thread::sleep_for(std::chrono::nanoseconds((int) nanoseconds));
-	return 1;
-}
+#if !defined( _MSC_VER)
+#define EXPORTED_FN __attribute__((visibility("default")))
+#else
+#define EXPORTED_FN __declspec(dllexport)
+#define snprintf sprintf_s
+#define strdup _strdup
+#endif
 
-DLL_EXPORT double sleep_microseconds(double microseconds) {
-	std::this_thread::sleep_for(std::chrono::microseconds((int) microseconds));
-	return 1;
-}
+extern "C" {
+    EXPORTED_FN double sleep_nanoseconds(double nanoseconds) {
+        std::this_thread::sleep_for(std::chrono::nanoseconds((int) nanoseconds));
+        return 1;
+    }
 
-DLL_EXPORT double sleep_milliseconds(double milliseconds) {
-	std::this_thread::sleep_for(std::chrono::milliseconds((int) milliseconds));
-	return 1;
-}
+    EXPORTED_FN double sleep_microseconds(double microseconds) {
+        std::this_thread::sleep_for(std::chrono::microseconds((int) microseconds));
+        return 1;
+    }
 
-DLL_EXPORT double sleep_seconds(double seconds) {
-	std::this_thread::sleep_for(std::chrono::seconds((int) seconds));
-	return 1;
-}
+    EXPORTED_FN double sleep_milliseconds(double milliseconds) {
+        std::this_thread::sleep_for(std::chrono::milliseconds((int) milliseconds));
+        return 1;
+    }
 
-DLL_EXPORT double sleep_minutes(double minutes) {
-	std::this_thread::sleep_for(std::chrono::minutes((int) minutes));
-	return 1;
+    EXPORTED_FN double sleep_seconds(double seconds) {
+        std::this_thread::sleep_for(std::chrono::seconds((int) seconds));
+        return 1;
+    }
+
+    EXPORTED_FN double sleep_minutes(double minutes) {
+        std::this_thread::sleep_for(std::chrono::minutes((int) minutes));
+        return 1;
+    }
 }
--- src/main.h
+++
@@ -1,8 +0,0 @@
-#include <thread>
-#include <chrono>
-
-#ifdef _WIN32
-#    define DLL_EXPORT extern "C" __declspec (dllexport)
-#else
-#    define DLL_EXPORT
-#endif
